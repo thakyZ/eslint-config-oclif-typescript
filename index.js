f6c76f461031ae49a0afaa115dd8450a97293727
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-08-22 15:17:28 -0700

    attempt to update

--- index.js
+++ index.js
@@ -1,58 +1,74 @@
-module.exports = {
-  extends: [
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "node:url";
+import globals from "globals";
+import js from "@eslint/js";
+import typescript from "typescript-eslint";
+
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  resolvePluginsRelativeTo: __dirname,
+});
+
+export default [
+  ...compat.extends([
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:n/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
     'plugin:perfectionist/recommended-natural',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-      },
-    ],
-    '@typescript-eslint/no-useless-constructor': 'error',
-    '@typescript-eslint/no-var-requires': 'off',
-    'import/no-unresolved': 'error',
-    'n/no-missing-import': 'off',
-    'n/no-unsupported-features/es-syntax': 'off',
-    'no-unused-expressions': 'off',
-    'no-useless-constructor': 'off',
-    'perfectionist/sort-classes': [
-      'error',
-      {
-        groups: [
-          'index-signature',
-          'static-property',
-          'property',
-          'private-property',
-          'constructor',
-          'static-method',
-          'static-private-method',
-          ['get-method', 'set-method'],
-          'method',
-          'private-method',
-          'unknown',
+    'plugin:jsdoc/recommended',
+  ]),
+  ...compat.plugins(['@typescript-eslint', 'import'])
+  ...compat.config(
+    {
+      parser: '@typescript-eslint/parser',
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+          },
         ],
-        order: 'asc',
-        type: 'alphabetical',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/no-var-requires': 'off',
+        'n/no-missing-import': 'off',
+        'n/no-unsupported-features/es-syntax': 'off',
+        'no-unused-expressions': 'off',
+        'no-useless-constructor': 'off',
+        'perfectionist/sort-classes': [
+          'error',
+          {
+            groups: [
+              'index-signature',
+              'static-property',
+              'property',
+              'private-property',
+              'constructor',
+              'static-method',
+              'static-private-method',
+              ['get-method', 'set-method'],
+              'method',
+              'private-method',
+              'unknown',
+            ],
+            order: 'asc',
+            type: 'alphabetical',
+          },
+        ],
+        'jsdoc/require-jsdoc': 'error',
       },
-    ],
-    'valid-jsdoc': ['warn', {requireParamType: false, requireReturnType: false}],
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+          typescript: {
+            alwaysTryTypes: true,
+          },
+        },
       },
-    },
-  },
-}
+    }
+  ),
+]
+
